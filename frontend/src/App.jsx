ad59a42fee08867589ff18f958bd01803a21f25c
Thales de Souza Fonseca <tsf.2007@gmail.com>
2025-02-15 17:50:23 -0300

    Adicionado integração com react-query para gerenciamento de dados de produtos e criação de API para requisições

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './index.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { QueryClient, QueryClientProvider  } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 // PAGES
 import Home from './pages/Home/Home';
@@ -11,22 +13,24 @@ import NavBar from './components/NavBar';
 
 
 
+const queryClient = new QueryClient()
 
 function App() {
 
 
   return (
-    <>
-    <Router>
-          <NavBar />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/contacts" element={<Contacts />} />
-        </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+            <NavBar />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/contacts" element={<Contacts />} />
+          </Routes>
 
-    </Router>
-    </>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
